df2e7292bcc9816d03d26306c1fa08e82b15cca5
Sibonisiwe Sodinga <sodinga91@gmail.com>
2020-08-04 18:21:23 +0200

    more changes on chulk and figlet

--- greet_xola.js
+++ greet_xola.js
@@ -1,12 +1,12 @@
 // const greet = require('./greet');
 // console.log(greet('Xola'))
 
-// const chalk = require('chalk');
-// const greet = require('./greet');
-// const styledMessage = chalk.bgGreen.black(greet('Xola'));
-// console.log(styledMessage)
-
 const chalk = require('chalk');
+const greet = require('./greet');
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+//const chalk = require('chalk');
 
 var figlet = require('figlet');
 figlet('Hello Xola!!', function(err, data) {
