2ac5b7bcb96d41c9cc22d59e16626f9b7aa15f9b
Sibonisiwe Sodinga <sodinga91@gmail.com>
2020-08-04 11:08:45 +0200

    added figlet

--- greet_xola.js
+++ greet_xola.js
@@ -1,7 +1,19 @@
 // const greet = require('./greet');
 // console.log(greet('Xola'))
 
-const chalk = require('chalk');
-const greet = require('./greet');
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+// const chalk = require('chalk');
+// const greet = require('./greet');
+// const styledMessage = chalk.bgGreen.black(greet('Xola'));
+// console.log(styledMessage)
+
+var figlet = require('figlet');
+var greet = require('./greet');
+figlet('Hello Xola!!', function(err, data) {
+    if (err){
+        console.log('something is not right...');
+        console.log.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
